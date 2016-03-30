efc375377636a8872c33bdbee2eb42548331107e
Sameer Naik <naikasameer@gmail.com>
2016-03-30 12:18:52 +0530

    Draft One

---
+++ MeetingApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MeetingApp.Startup))]
+namespace MeetingApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
